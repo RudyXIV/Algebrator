8e3bbacee9630cf0b4e9398def071d0a29d27009
Luca Camphuisen <lucamasira@gmail.com>
2014-08-31 00:56:57 +0200

    Started on UI

---
+++ src/JavaProphet/algebrator/ui/AlgebratorUI.java
@@ -0,0 +1,21 @@
+package JavaProphet.algebrator.ui;
+
+import javax.swing.*;
+import javax.swing.plaf.DimensionUIResource;
+import java.awt.*;
+
+/**
+ * Created by Luca on 8/31/2014.
+ */
+public class AlgebratorUI extends JFrame {
+
+    public AlgebratorUI() {
+        //initialization of the ui
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(new Dimension(500,500));
+        setVisible(true);
+        setTitle("Algebrator");
+        setLayout(null);
+        //will do UImanager laterr
+    }
+}
