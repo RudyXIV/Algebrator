6f08c487c55e86e5f94b728abad07b5fa88b03f1
JavaProphet <max.bruce12@gmail.com>
2014-08-30 15:35:03 -0700

    More work on parsing/debug output.

--- Algebrator/src/MainTest.java
+++ Algebrator/src/MainTest.java
@@ -1,10 +1,16 @@
+import javaprophet.algebrator.parser.Equation;
+import javaprophet.algebrator.parser.Parse;
+
 /**
  * Created by JavaProphet on 8/30/14 at 12:36 PM.
  */
 public class MainTest {
 
 	public static void main(String[] args) {
-		System.out.println("gittest");
+		Parse parse = new Parse();
+		Equation e = parse.parse("2x+7=9*7");
+		System.out.println(e.listTermsForSet(0));
+		System.out.println(e.listTermsForSet(1));
 	}
 
 }
