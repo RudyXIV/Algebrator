716ed66be083702ef1ea1979018fb5339e02bf48
JavaProphet <max.bruce12@gmail.com>
2014-08-30 12:50:41 -0700

    Added Parser file structure.

---
+++ Algebrator/src/JavaProphet/algebrator/parser/Equation.java
@@ -0,0 +1,7 @@
+package JavaProphet.algebrator.parser;
+
+/**
+ * Created by JavaProphet on 8/30/14 at 12:49 PM.
+ */
+public class Equation {
+}
---
+++ Algebrator/src/JavaProphet/algebrator/parser/Parse.java
@@ -0,0 +1,7 @@
+package JavaProphet.algebrator.parser;
+
+/**
+ * Created by JavaProphet on 8/30/14 at 12:48 PM.
+ */
+public class Parse {
+}
