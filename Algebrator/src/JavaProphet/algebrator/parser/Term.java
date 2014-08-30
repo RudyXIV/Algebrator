37c78f90b0223bb846501d2714b2452e92f05e1a
JavaProphet <max.bruce12@gmail.com>
2014-08-30 15:07:07 -0700

    Added Term update 1

--- Algebrator/src/JavaProphet/algebrator/parser/Term.java
+++ Algebrator/src/JavaProphet/algebrator/parser/Term.java
@@ -4,4 +4,16 @@ package JavaProphet.algebrator.parser;
  * Created by JavaProphet on 8/30/14 at 12:48 PM.
  */
 public class Term {
+
+	private String raw = "";
+	//private HashMap<>
+
+	public Term(String raw) {
+		this.raw = raw;
+	}
+
+	public boolean hasVariable() {
+
+	}
+
 }
